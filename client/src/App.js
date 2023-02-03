cb652cde0c354675a8730e33d64063fd0c8b2028
Henri Vähätalo <henri.vahatalo@tuni.fi>
2023-02-03 11:44:12 +0200

    client add item

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,27 @@
 import './App.css';
+import React from "react";
+import { useState, useEffect, useContext, useCallback } from "react";
 
 function App() {
+
+  const [todoList, setTodoList] = useState([]);
+  const [input, setInput] = useState("");
+
+  const add_comment = () => {
+    setTodoList(arr => [...arr, `${input}`]);
+    setInput("");
+  }
+
+  const textChanged = (event) => {
+    setInput(event.target.value);
+  }
+
   return (
     <div className="App">
       <h1>CLIENT</h1>
+      <textarea id="input_field" rows="3" cols="50" value={input} onChange={textChanged}></textarea>
+      <br></br><button id="add_button" onClick={add_comment}>Add task</button>
+      <div>{todoList.map(item => <div key={item}>{item}</div>)}</div>
     </div>
   );
 }
