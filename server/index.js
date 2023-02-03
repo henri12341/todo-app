24016772c917f7428517be590b98bc2ab62592bf
Henri Vähätalo <henri.vahatalo@tuni.fi>
2023-02-03 14:10:31 +0200

    server start

--- server/index.js
+++ server/index.js
@@ -1,5 +1,41 @@
 const sqlite3 = require('sqlite3').verbose();
+const express = require("express")
+const sqlite = require("better-sqlite3")
+const path = require('path');
+const db = new sqlite(path.resolve("todo_app.db"), {fileMustExist: true});
 
+const app = express()
+app.use(express.json());
+
+
+
+app.use( (req,res,next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Authorization')
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE')
+    next();
+})
+
+
+app.get("", async (req,res,next) => {
+    res.json("Server page")
+})
+
+app.get("/get_tasks", async (req,res,next) => {
+    let data = await db.prepare("SELECT * FROM tasks").all()
+    console.log(data)
+    res.json(data)
+})
+
+const port = process.env.PORT || 5000
+if (process.env.NODE_ENV !== 'test') {
+    app.listen(port, () => {
+        console.log(`Api is running on port ${port}`)
+    })
+}
+
+
+/*
 let db = new sqlite3.Database('./todo_app.db', sqlite3.OPEN_READWRITE ,(err) => {
   if (err) {
     console.error(err.message);
@@ -45,3 +81,4 @@ db.close((err) => {
     }
     console.log("CLosed database connection")
 })
+*/
