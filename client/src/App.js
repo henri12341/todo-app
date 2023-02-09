a42f7d1af6736460e2561eb40ddc5a6ec4dcf4e5
Henri Vähätalo <henri.vahatalo@tuni.fi>
2023-02-09 22:50:25 +0200

    modify css

--- client/src/App.js
+++ client/src/App.js
@@ -39,9 +39,13 @@ function App() {
   return (
     <div className="App">
       <h1>TODO APP</h1>
-      <textarea id="input_field" rows="3" cols="50" value={input} onChange={textChanged}></textarea>
+      <div class="textarea-wrapper">
+        <textarea id="input_field" rows="3" cols="50" value={input} onChange={textChanged}></textarea>
+      </div>
       <br></br><button id="add_button" onClick={add_task}>Add task</button><br></br><br></br>
-      <div>{todoList.map(item => <div key={item.id}>{item.item} <button id={item.id} onClick={delete_task}>delete</button></div>)} </div>
+      <table>
+          {todoList.map(item => <tr key={item.id}>{item.item} <button id={item.id} class="delete" onClick={delete_task}>X</button></tr>)}
+      </table>
     </div>
   );
 }
