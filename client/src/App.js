e55c5f4805baf80a95d8f459f0651d2d9113f644
Henri Vähätalo <henri.vahatalo@tuni.fi>
2023-02-10 11:55:00 +0200

    update css

--- client/src/App.js
+++ client/src/App.js
@@ -43,9 +43,9 @@ function App() {
         <textarea id="input_field" rows="3" cols="50" value={input} onChange={textChanged}></textarea>
       </div>
       <br></br><button id="add_button" onClick={add_task}>Add task</button><br></br><br></br>
-      <table>
-          {todoList.map(item => <tr key={item.id}>{item.item} <button id={item.id} class="delete" onClick={delete_task}>X</button></tr>)}
-      </table>
+      <ul>
+          {todoList.map(item => <li key={item.id}>{item.item} <button id={item.id} class="delete" onClick={delete_task}>X</button></li>)}
+      </ul>
     </div>
   );
 }
